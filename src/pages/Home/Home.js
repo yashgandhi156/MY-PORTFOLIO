990518ce670f1a91bfac51e1ea3e37a848e7eeb5
yash <yashgandhi156@gmail.com>
2023-12-03 14:21:49 +0530

    Home Section

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,10 +1,39 @@
 import React from "react";
+import Typewriter from "typewriter-effect";
 import "./home.css";
+import Resume from "../../assets/docs/Resume.pdf";
 const Home = () => {
   return (
-    <div>
-      <h1>Home Page</h1>
-    </div>
+    <>
+      <div className="container-fluid home-container">
+        <div className="container home-content">
+          <h1>Hi I'm a</h1>
+          <h2>
+            <Typewriter
+              options={{
+                strings: [
+                  "FullStack Developer!",
+                  "Mern Stack Developer!",
+                  "React native developer!",
+                ],
+                autoStart: true,
+                loop: true,
+              }}
+            />
+          </h2>
+          <div className="home-buttons">
+            <button className="btn btn-hire">Hire Me</button>
+            <a
+              className="btn btn-cv"
+              href={Resume}
+              download="21145098_yash.pdf"
+            >
+              My Resume
+            </a>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
