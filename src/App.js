0a9409033451de26727904dc8a36973e42508509
yash <yashgandhi156@gmail.com>
2023-12-06 14:20:40 +0530

    Contact Section

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Layout from "./components/Layout/Layout";
 import About from "./pages/About/About";
+import Contact from "./pages/Contact/Contact";
 import Education from "./pages/Education/Education";
 import Project from "./pages/Projects/Project";
 import Techstack from "./pages/Techstack/Techstack";
@@ -15,6 +16,10 @@ function App() {
         <Techstack />
         <Project />
         <WorkExp />
+        <Contact />
+      </div>
+      <div className="footer pb-3 ms-3">
+        <h4 className="text-center">Made With 😍 Yash Gandhi &copy; 2023</h4>
       </div>
     </div>
   );
