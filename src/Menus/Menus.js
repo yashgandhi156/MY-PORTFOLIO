f84ba4f3d90c77cc490d7e5e0b558827e9531506
yash <yashgandhi156@gmail.com>
2023-12-09 19:34:01 +0530

    Reactjs animation effect

--- src/Menus/Menus.js
+++ src/Menus/Menus.js
@@ -1,5 +1,7 @@
 import React from "react";
 import "./Menus.css";
+import Zoom from "react-reveal/Zoom";
+import Fade from "react-reveal/Fade";
 import { Link } from "react-scroll";
 import {
   FcHome,
@@ -15,102 +17,106 @@ const Menus = ({ toggle }) => {
     <>
       {toggle ? (
         <>
-          <div className="navbar-profile-pic">
-            <img
-              src="https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-              alt="profile pic"
-            />
-          </div>
-          <div className="nav-items">
-            <div className="nav-item">
-              <div className="nav-link">
-                <Link
-                  to="home"
-                  spy={true}
-                  smooth={true}
-                  offset={-100}
-                  duration={100}
-                >
-                  <FcHome />
-                  Home
-                </Link>
-              </div>
-              <div className="nav-link">
-                <Link
-                  to="about"
-                  spy={true}
-                  smooth={true}
-                  offset={-100}
-                  duration={100}
-                >
-                  <FcAbout />
-                  About
-                </Link>
-              </div>
-              <div className="nav-link">
-                <Link
-                  to="education"
-                  spy={true}
-                  smooth={true}
-                  offset={-100}
-                  duration={100}
-                >
-                  <FcReadingEbook />
-                  Education
-                </Link>
-              </div>
+          <Zoom>
+            <div className="navbar-profile-pic">
+              <img
+                src="https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+                alt="profile pic"
+              />
+            </div>
+          </Zoom>
+          <Fade left>
+            <div className="nav-items">
+              <div className="nav-item">
+                <div className="nav-link">
+                  <Link
+                    to="home"
+                    spy={true}
+                    smooth={true}
+                    offset={-100}
+                    duration={100}
+                  >
+                    <FcHome />
+                    Home
+                  </Link>
+                </div>
+                <div className="nav-link">
+                  <Link
+                    to="about"
+                    spy={true}
+                    smooth={true}
+                    offset={-100}
+                    duration={100}
+                  >
+                    <FcAbout />
+                    About
+                  </Link>
+                </div>
+                <div className="nav-link">
+                  <Link
+                    to="education"
+                    spy={true}
+                    smooth={true}
+                    offset={-100}
+                    duration={100}
+                  >
+                    <FcReadingEbook />
+                    Education
+                  </Link>
+                </div>
 
-              <div className="nav-link">
-                <Link
-                  to="techstack"
-                  spy={true}
-                  smooth={true}
-                  offset={-100}
-                  duration={100}
-                >
-                  <FcBiotech />
-                  Tech Stack
-                </Link>
-              </div>
+                <div className="nav-link">
+                  <Link
+                    to="techstack"
+                    spy={true}
+                    smooth={true}
+                    offset={-100}
+                    duration={100}
+                  >
+                    <FcBiotech />
+                    Tech Stack
+                  </Link>
+                </div>
 
-              <div className="nav-link">
-                <Link
-                  to="projects"
-                  spy={true}
-                  smooth={true}
-                  offset={-100}
-                  duration={100}
-                >
-                  <FcVideoProjector />
-                  Projects
-                </Link>
-              </div>
-              <div className="nav-link">
-                <Link
-                  to="work"
-                  spy={true}
-                  smooth={true}
-                  offset={-100}
-                  duration={100}
-                >
-                  <FcPortraitMode />
-                  Work Experince
-                </Link>
-              </div>
-              <div className="nav-link">
-                <Link
-                  to="contact"
-                  spy={true}
-                  smooth={true}
-                  offset={-100}
-                  duration={100}
-                >
-                  <FcBusinessContact />
-                  Contact
-                </Link>
+                <div className="nav-link">
+                  <Link
+                    to="projects"
+                    spy={true}
+                    smooth={true}
+                    offset={-100}
+                    duration={100}
+                  >
+                    <FcVideoProjector />
+                    Projects
+                  </Link>
+                </div>
+                <div className="nav-link">
+                  <Link
+                    to="work"
+                    spy={true}
+                    smooth={true}
+                    offset={-100}
+                    duration={100}
+                  >
+                    <FcPortraitMode />
+                    Work Experince
+                  </Link>
+                </div>
+                <div className="nav-link">
+                  <Link
+                    to="contact"
+                    spy={true}
+                    smooth={true}
+                    offset={-100}
+                    duration={100}
+                  >
+                    <FcBusinessContact />
+                    Contact
+                  </Link>
+                </div>
               </div>
             </div>
-          </div>
+          </Fade>
         </>
       ) : (
         <>
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -4,6 +4,7 @@ import Typewriter from "typewriter-effect";
 import "./home.css";
 import Resume from "../../assets/docs/Resume.pdf";
 import { BsFillMoonStarsFill, BsFillSunFill } from "react-icons/bs";
+import Fade from "react-reveal/Fade";
 const Home = () => {
   const [theme, setTheme] = useTheme();
   //handle theme
@@ -21,30 +22,37 @@ const Home = () => {
           )}
         </div>
         <div className="container home-content">
-          <h2>Hi 👋 I'm a</h2>
-          <h1>
-            <Typewriter
-              options={{
-                strings: [
-                  "FullStack Developer!",
-                  "Mern Stack Developer!",
-                  "React native developer!",
-                ],
-                autoStart: true,
-                loop: true,
-              }}
-            />
-          </h1>
-          <div className="home-buttons">
-            <button className="btn btn-hire">Hire Me</button>
-            <a
-              className="btn btn-cv"
-              href={Resume}
-              download="21145098_yash.pdf"
-            >
-              My Resume
-            </a>
-          </div>
+          <Fade right>
+            <h2>Hi 👋 I'm a</h2>
+            <h1>
+              <Typewriter
+                options={{
+                  strings: [
+                    "FullStack Developer!",
+                    "Mern Stack Developer!",
+                    "React native developer!",
+                  ],
+                  autoStart: true,
+                  loop: true,
+                }}
+              />
+            </h1>
+          </Fade>
+          <Fade bottom>
+            <div className="home-buttons">
+              <a
+                className="btn btn-hire"
+                href="https://api.whatsapp.com/send?phone=1234567890"
+                rel="noreferrer"
+                target="_blank"
+              >
+                Hire Me
+              </a>
+              <a className="btn btn-cv" href={Resume} download="your_name.pdf">
+                My Resume
+              </a>
+            </div>
+          </Fade>
         </div>
       </div>
     </>
